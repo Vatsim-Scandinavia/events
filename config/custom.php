ba77c6ab45d3def880b45515c636bcc5a8555d99
Marko259 <animationsisnice@gmail.com>
2024-07-22 19:38:23 +0200

    Removed unused config vars and added github actions

--- config/custom.php
+++ config/custom.php
@@ -7,13 +7,6 @@ return [
     |--------------------------------------------------------------------------
     |
     */
-    'cc_url' => env('CC_URL', null),
-    'cc_api_secret' => env('CC_API_SECRET', null),
-
-    'forum_api_url' => env('FORUM_API_URL', null),
-    'forum_calendar_type' => env('FORUM_CALENDAR_TYPE', null),
-    'forum_api_secret' => env('FORUM_API_SECRET', null),
-
     'discord_bot_token' => env('DISCORD_BOT_TOKEN', null),
     'discord_guild_id' => env('DISCORD_GUILD_ID', null),
 
