838db79c888fd2359db2a11eeb3955ac986a9a4f
Daniel <blt950@users.noreply.github.com>
2024-07-30 19:02:28 +0200

    fix: building image workflow

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -7,12 +7,12 @@ SELF_SIGNED_CERT=/etc/ssl/certs/apache-selfsigned.crt
 
 if [ ! -f "$SELF_SIGNED_KEY" ] || [ ! -f "$SELF_SIGNED_CERT" ]; then
     # Generate a self-signed cert to support SSL connections
-    openssl req -x509 -nodes -days 358000 -newkey rsa:2048 -keyout "$SELF_SIGNED_KEY" -out "$SELF_SIGNED_CERT" -subj "/O=Your vACC/CN=Control Center"
+    openssl req -x509 -nodes -days 358000 -newkey rsa:2048 -keyout "$SELF_SIGNED_KEY" -out "$SELF_SIGNED_CERT" -subj "/O=Your vACC/CN=Events"
 fi
 
 if [ -z "$APP_KEY" ] && [ ! -f "$CONTROL_CENTER_ROOT/.env" ]; then
     echo "################################################################################"
-    echo "WARNING: You need to follow the configuration guide for Control Center"
+    echo "WARNING: You need to follow the configuration guide"
     echo "################################################################################"
     echo "WARNING: Copying over example .env file"
     cp container/example-prod.env .env
