b92f8f5cd6cba55370df9fc5ea8722caee8aa98d
Marko259 <animationsisnice@gmail.com>
2025-04-02 20:39:31 +0200

    Migrations

--- database/migrations/2025_04_02_183015_modify_staffings_id.php
+++ database/migrations/2025_04_02_183015_modify_staffings_id.php
@@ -11,15 +11,23 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('positions', function (Blueprint $table) {
-            $table->dropForeign(['staffing_id']);
-        });
+        Schema::drop('positions');
 
         Schema::table('staffings', function (Blueprint $table) {
             $table->unsignedInteger('id')->autoIncrement()->change();
         });
 
-        Schema::table('positions', function (Blueprint $table) {
+        Schema::create('positions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('callsign');
+            $table->bigInteger('booking_id')->nullable();
+            $table->bigInteger('discord_user')->nullable();
+            $table->integer('section');
+            $table->boolean('local_booking')->default(false);
+            $table->time('start_time')->nullable();
+            $table->time('end_time')->nullable();
+            $table->unsignedInteger('staffing_id');
+            $table->timestamps();
             $table->foreign('staffing_id')->references('id')->on('staffings')->onDelete('cascade');
         });
     }
