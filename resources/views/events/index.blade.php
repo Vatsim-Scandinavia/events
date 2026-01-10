34767389b60906a8b5c9b16ab407248fa639bdf0
Benjamin Jørgensen <benjamin@dunkstormen.dk>
2026-01-10 13:15:46 +0100

    Fix: Refactor of events index to use the logic/schema.

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -24,11 +24,10 @@
                             data-page-size="10" data-toggle="table" data-pagination="true" data-filter-control="true" data-sort-reset="true" data-sort-select-options="true">
                             <thead class="table-light">
                                 <tr>
-                                    <th data-field="id" data-sortable="true" data-filter-control="input">ID</th>
                                     <th data-field="title" data-sortable="true" data-filter-control="input">Title</th>
                                     <th data-field="start_date" data-sortable="true" data-filter-control="input">Start Date</th>
                                     <th data-field="end_date" data-sortable="true" data-filter-control="input">End Date</th>
-                                    <th data-field="parent" data-sortable="true" data-filter-control="input">Parent Event</th>
+                                    <th data-field="recurring" data-sortable="true" data-filter-control="select">Recurring</th>
                                     <th data-field="calendar" data-sortable="true" data-filter-control="select">Calendar</th>
                                     <th data-field="createdby" data-sortable="true" data-filter-control="select">Created by</th>
                                     <th data-field="actions" data-sortable="false" data-filter-control="false">Actions</th>
@@ -38,33 +37,53 @@
                                 @foreach ($events as $event)
                                     @can('view', $event)
                                         <tr>
-                                            <td>{{ $event->id }}</td>
                                             <td>{{ $event->title }}</td>
-                                            <td>{{ \Carbon\Carbon::parse($event->start_date)->format('d-m-Y H:i') }}z</td>
-                                            <td>{{ \Carbon\Carbon::parse($event->end_date)->format('d-m-Y H:i') }}z</td>
-                                            <td class="text-center text-white">
-                                                @if ($event->children->isNotEmpty())
-                                                    <p class="text-success mb-0">Parent</p>
-                                                @elseif($event->parent_id != null)
-                                                    <a href="{{ route('events.edit', $event->parent) }}" class="flex-grow-1">{{ $event->parent->title }}</a>
-                                                    <p class="text-muted mt-auto">Id: {{ $event->parent_id }}</p>
+                                            <td>
+                                                @if($event->nextInstance)
+                                                    {{ $event->nextInstance->start_time->format('d-m-Y H:i') }}z
+                                                @else
+                                                    <span class="text-muted">TBD</span>
                                                 @endif
                                             </td>
+                                            <td>
+                                                @if($event->nextInstance)
+                                                    {{ $event->nextInstance->end_time->format('d-m-Y H:i') }}z
+                                                @else
+                                                    <span class="text-muted">TBD</span>
+                                                @endif
+                                            </td>
+                                            <td>{{ $event->recurrence_interval ? 'Yes' : 'No' }}</td>
                                             <td>{{ $event->calendar->name }}</td>
                                             <td>{{ $event->user->name }}</td>
                                             <td>
-                                                <a class="btn btn-sm btn-info" href="{{ route('events.show', $event->id) }}"><i class="fas fa-eye"></i> Show</a>
-                                                @can('update', $event)
-                                                    <a class="btn btn-sm btn-primary" href="{{ route('events.edit', $event->id) }}"><i class="fas fa-edit"></i> Edit</a>
-                                                @endcan
-                                                @can('destroy', $event)
-                                                    <form method="POST" action="{{ route('events.destroy', $event->id) }}" style="display:inline"
-                                                        onsubmit="return confirm('Are you sure you want to delete this event? - {{ $event->title }}')">
-                                                        @method('DELETE')
-                                                        @csrf
-                                                        <button class="btn btn-sm btn-danger" type="submit"><i class="fas fa-trash" aria-hidden="true"></i> Delete @if ($event->children->isNotEmpty()) all recurring  @elseif($event->parent_id != null) occurance @endif</button>
-                                                    </form>
-                                                @endcan
+                                                <div class="d-flex flex-column" style="gap: 0.25rem;">
+                                                    @if($event->nextInstance)
+                                                        <a class="btn btn-sm btn-info" href="{{ route('events.show', ['event' => $event->id, 'instance' => $event->nextInstance->id]) }}">
+                                                            <i class="fas fa-eye fa-fw"></i> Show
+                                                        </a>
+                                                    @else
+                                                        <a class="btn btn-sm btn-info disabled" href="#">
+                                                            <i class="fas fa-eye fa-fw"></i> No occurrences
+                                                        </a>
+                                                    @endif
+
+                                                    @can('update', $event)
+                                                        <a class="btn btn-sm btn-primary" href="{{ route('events.edit', $event->id) }}">
+                                                            <i class="fas fa-edit fa-fw"></i> Edit
+                                                        </a>
+                                                    @endcan
+
+                                                    @can('destroy', $event)
+                                                        <form method="POST" action="{{ route('events.destroy', $event->id) }}" class="d-grid"
+                                                            onsubmit="return confirm('Are you sure you want to delete this event? - {{ $event->title }}')">
+                                                            @method('DELETE')
+                                                            @csrf
+                                                            <button class="btn btn-sm btn-danger" type="submit">
+                                                                <i class="fas fa-trash fa-fw" aria-hidden="true"></i> Delete
+                                                            </button>
+                                                        </form>
+                                                    @endcan
+                                                </div>
                                             </td>
                                         </tr>
                                     @endcan
