630ce83470a1c35979455acb58bf4f2c1573f1f5
Benjamin Jørgensen <benjamin@dunkstormen.dk>
2026-01-11 23:19:55 +0100

    Fix: Added use for EventInstance relationship.

--- app/Models/Calendar.php
+++ app/Models/Calendar.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\EventInstance;
+
 
 class Calendar extends Model
 {
@@ -15,7 +17,7 @@ class Calendar extends Model
         'public',
     ];
 
-    public function instances()
+    public function instances(): \Illuminate\Database\Eloquent\Relations\HasManyThrough
     {
         return $this->hasManyThrough(EventInstance::class, Event::class);
     }
