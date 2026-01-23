de91a4fd5b846bb51ca4c033919c4a04a46e5e1a
Benjamin Henning Bruun Jørgensen <benjamin@dunkstormen.dk>
2026-01-23 10:27:53 +0000

    feat: Staffing resource

---
+++ app/Http/Resources/StaffingResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class StaffingResource extends JsonResource
+{
+    protected $targetOccurrenceDate;
+
+    public function __construct($resource, $targetOccurrenceDate = null)
+    {
+        parent::__construct($resource);
+        $this->targetOccurrenceDate = $targetOccurrenceDate;
+    }
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'discord' => [
+                'channel_id' => $this->discord_staffing_channel_id,
+                'message_id' => $this->discord_staffing_message_id,
+            ],
+        ];
+    }
+}
