a0b01609d9167304440ce465bf39a8f6da89056a
Benjamin Henning Bruun Jørgensen <benjamin@dunkstormen.dk>
2026-01-24 15:40:42 +0000

    fix: Ammended EventResource

--- app/Http/Resources/EventResource.php
+++ app/Http/Resources/EventResource.php
@@ -14,7 +14,6 @@ class EventResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $isAuthenticated = $request->attributes->get('api_key');
 
         return [
             'id' => $this->id,
@@ -23,16 +22,11 @@ class EventResource extends JsonResource
             'long_description' => $this->long_description,
             'start_datetime' => $this->start_datetime->toIso8601String(),
             'end_datetime' => $this->end_datetime->toIso8601String(),
+            'start_date' => $this->start_datetime->toDateString('Y-m-d H:i:s'),
+            'end_date' => $this->end_datetime->toDateString('Y-m-d H:i:s'),
             'featured_airports' => $this->featured_airports ?? [],
             'banner' => $this->banner ? asset('storage/' . $this->banner) : null,
             'uri' => url('/events/' . $this->id),
-
-            $this->mergeWhen($isAuthenticated, [
-                'discord' => [
-                    'channel_id' => $this->discord_staffing_channel_id,
-                    'message_id' => $this->discord_staffing_message_id,
-                ]
-            ]),
         ];
     }
 }
